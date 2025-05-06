59c00ab707cd8b8ab5ebc86a334567a7662f4802
seucarrousado <eversonmenezes2018@gmail.com>
2025-05-06 17:49:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-async def home():
-    return {"mensagem": "API da Tabela FIPE funcionando corretamente"}
+def read_root():
+    return {"mensagem": "API FIPE funcionando!"}
+
 
 
 
