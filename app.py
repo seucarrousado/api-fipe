43cef176d2a7448fa63611b637537b924c70acec
seucarrousado <eversonmenezes2018@gmail.com>
2025-05-06 17:36:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
-from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
 @app.get("/")
-async def root():
-    return JSONResponse(content={"mensagem": "API da Tabela FIPE funcionando corretamente"})
+def read_root():
+    return {"mensagem": "API da Tabela FIPE funcionando corretamente"}
+
