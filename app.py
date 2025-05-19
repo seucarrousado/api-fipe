26d7bb1cc5dac3fa4fa485fce5ca23b8c7361977
seucarrousado <eversonmenezes2018@gmail.com>
2025-05-19 12:03:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -131,8 +131,11 @@ async def buscar_precos_pecas(marca: str, modelo: str, ano: str, pecas: str = Qu
         marca_nome = await obter_nome_marca(marca)
         modelo_nome = await obter_nome_modelo(modelo)
 
-        # Correção para o ano - remove qualquer sufixo após o hífen
-        ano_nome = ano.split("-")[0] if "-" in ano else ano
+        # Regra explícita de conversão do ano
+        if "-" in ano and ano.split("-")[1] == "1":
+            ano_nome = ano.split("-")[0]
+        else:
+            ano_nome = ano
 
         relatorio, total_abatido = await buscar_precos_e_gerar_relatorio(
             marca_nome, modelo_nome, ano_nome, lista_pecas
